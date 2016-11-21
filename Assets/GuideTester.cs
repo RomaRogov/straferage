f718c119962d347955bc96405b48e9e96bdfac0e
Роман Рогов <roman.rogov@nevosoft.ru>
2016-11-21 20:41:07 +0300

    some new curves

---
+++ Assets/GuideTester.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class GuideTester : MonoBehaviour
+{
+    private float guideRotation;
+
+    public string GuideName;
+
+    void Update ()
+    {
+        BezierCurve guide = null;
+        Array.ForEach(Physics.OverlapSphere(transform.position, 1f), col => { if (col.isTrigger) { guide = col.GetComponent<BezierCurve>(); } });
+
+        if (guide != null)
+        {
+            guideRotation = Mathf.LerpAngle(guideRotation, Quaternion.LookRotation(guide.GetDirection(guide.GetPointValue(transform.position))).eulerAngles.y, Time.fixedDeltaTime * 10f);
+            GuideName = guide.name;
+        }
+        else
+        {
+            GuideName = "NULL";
+        }
+        transform.rotation = Quaternion.Euler(Vector3.up * (guideRotation));
+    }
+}
