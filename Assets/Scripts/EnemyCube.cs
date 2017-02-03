4ce7c5c7a4bb816186311fb326fd20df308a22cb
bL00RiSe <hoha2005@yandex.ru>
2017-02-03 12:11:39 +0300

    enemies?

--- Assets/Scripts/EnemyCube.cs
+++ Assets/Scripts/EnemyCube.cs
@@ -4,11 +4,35 @@ using System.Collections;
 
 public class EnemyCube : Enemy {
 
+    public Transform[] WayPoints;
+
+    private Rigidbody rbd;
+    private Transform currentPoint { get { return WayPoints[currentPointIndex]; } }
+    private int currentPointIndex = 0;
 	
 	void Start ()
     {
-	
+        rbd = GetComponent<Rigidbody>();
 	}
+
+    void Update()
+    {
+        Vector3 dir = (currentPoint.position - transform.position).normalized;
+        dir.y = 0f;
+        rbd.AddForce(dir * 20f);
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col == currentPoint.GetComponent<Collider>())
+        {
+            currentPointIndex++;
+            if (currentPointIndex >= WayPoints.Length)
+            {
+                currentPointIndex = 0;
+            }
+        }
+    }
 
     protected override void OnDied()
     {
